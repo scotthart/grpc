cf107439461cebaf9de74cb78f14b149954b2b57
scotthart <17603278+scotthart@users.noreply.github.com>
2026-06-02 20:54:01 +0000

    Automated change: Fix sanity tests

--- src/core/credentials/call/external/gdch_service_account_credentials.cc
+++ src/core/credentials/call/external/gdch_service_account_credentials.cc
@@ -32,7 +32,6 @@
 #include <utility>
 
 #include "src/core/credentials/call/call_credentials.h"
-#include "src/core/util/sync.h"
 #include "src/core/credentials/call/json_util.h"
 #include "src/core/credentials/transport/transport_credentials.h"
 #include "src/core/lib/iomgr/closure.h"
@@ -42,6 +41,7 @@
 #include "src/core/util/http_client/parser.h"
 #include "src/core/util/json/json.h"
 #include "src/core/util/json/json_reader.h"
+#include "src/core/util/sync.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
@@ -363,8 +363,8 @@ GDCHServiceAccountCredentials::RetrieveSubjectToken(
 #endif
 
 std::pair<std::string, std::string>
-GDCHServiceAccountCredentials::AssertionComponentsFromInfo(Info const& info,
-                                                           std::chrono::system_clock::time_point now) {
+GDCHServiceAccountCredentials::AssertionComponentsFromInfo(
+    Info const& info, std::chrono::system_clock::time_point now) {
   Json header = Json::FromObject({
       {"alg", Json::FromString("ES256")},
       {"typ", Json::FromString("JWT")},
@@ -378,7 +378,8 @@ GDCHServiceAccountCredentials::AssertionComponentsFromInfo(Info const& info,
   auto const expiration_from_epoch = static_cast<std::intmax_t>(
       std::chrono::system_clock::to_time_t(expiration));
   std::cout << __func__ << ": now_from_epoch=" << now_from_epoch << std::endl;
-  std::cout << __func__ << ": expiration_from_epoch=" << expiration_from_epoch << std::endl; 
+  std::cout << __func__ << ": expiration_from_epoch=" << expiration_from_epoch
+            << std::endl;
 
 #if 0
   // Resulting access token should expire after one hour.
@@ -387,7 +388,7 @@ GDCHServiceAccountCredentials::AssertionComponentsFromInfo(Info const& info,
   gpr_timespec expiration = gpr_time_add(now_realtime, token_lifetime);
 
   std::cout << __func__ << ": now_realtime.tv_sec=" << now_realtime.tv_sec << std::endl;
-  std::cout << __func__ << ": expiration.tv_sec=" << expiration.tv_sec << std::endl; 
+  std::cout << __func__ << ": expiration.tv_sec=" << expiration.tv_sec << std::endl;
 #endif
   auto iss_sub_value = absl::StrCat("system:serviceaccount:", info.project_id,
                                     ":", info.service_identity_name);
@@ -454,7 +455,8 @@ absl::StatusOr<std::string> GDCHServiceAccountCredentials::CreateRequestBody(
   // gpr_timespec now = gpr_now(GPR_CLOCK_REALTIME);
   // auto now = std::chrono::system_clock::now();
 
-  auto [header, claim] = AssertionComponentsFromInfo(info, std::chrono::system_clock::now());
+  auto [header, claim] =
+      AssertionComponentsFromInfo(info, std::chrono::system_clock::now());
   std::cout << __func__ << ": header=\n" << header << std::endl;
   std::cout << __func__ << ": claim=\n" << claim << std::endl;
   auto jwt =
@@ -636,13 +638,12 @@ class GDCHServiceAccountCredentials::GDCHFetchRequest final
  public:
   GDCHFetchRequest(
       GDCHServiceAccountCredentials* creds, Timestamp deadline,
-      absl::AnyInvocable<void(
-          absl::StatusOr<RefCountedPtr<TokenFetcherCredentials::Token>>)>
+      absl::AnyInvocable<
+          void(absl::StatusOr<RefCountedPtr<TokenFetcherCredentials::Token>>)>
           on_done)
       : creds_(creds), on_done_(std::move(on_done)) {
     fetch_body_ = creds_->RetrieveSubjectToken(
-        deadline,
-        [this](absl::StatusOr<std::string> result) {
+        deadline, [this](absl::StatusOr<std::string> result) {
           OnSubjectToken(std::move(result));
         });
   }
--- test/core/credentials/call/call_credentials_test.cc
+++ test/core/credentials/call/call_credentials_test.cc
@@ -72,6 +72,7 @@
 #include "src/core/util/unique_type_name.h"
 #include "src/core/util/uri.h"
 #include "src/core/util/wait_for_single_owner.h"
+#include "test/core/credentials/call/oauth2/oauth2_utils.h"
 #include "test/core/event_engine/event_engine_test_utils.h"
 #include "test/core/event_engine/fuzzing_event_engine/fuzzing_event_engine.h"
 #include "test/core/test_util/test_call_creds.h"
@@ -84,8 +85,6 @@
 #include "absl/strings/str_format.h"
 #include "absl/strings/str_replace.h"
 #include "absl/strings/str_split.h"
-
-#include "test/core/credentials/call/oauth2/oauth2_utils.h"
 
 // TODO(roth): Refactor this so that we can split up the individual call
 // creds tests into their own files.
@@ -4758,7 +4757,8 @@ int gdch_service_account_creds_httpcli_post_success(
   EXPECT_TRUE(parsed_body.ok()) << parsed_body.status().ToString();
   EXPECT_EQ(parsed_body->object().at("grant_type").string(),
             "urn:ietf:params:oauth:token-type:token-exchange");
-  EXPECT_EQ(parsed_body->object().at("audience").string(), "https://my-audience.com");
+  EXPECT_EQ(parsed_body->object().at("audience").string(),
+            "https://my-audience.com");
   EXPECT_EQ(parsed_body->object().at("requested_token_type").string(),
             "urn:ietf:params:oauth:token-type:access_token");
   EXPECT_EQ(parsed_body->object().at("subject_token_type").string(),
@@ -4768,13 +4768,13 @@ int gdch_service_account_creds_httpcli_post_success(
   std::vector<std::string> parts = absl::StrSplit(jwt_token, '.');
   EXPECT_EQ(parts.size(), 3);
 
-  *response = http_response(200, "{\"access_token\": \"my-exchanged-gdch-token\"}");
+  *response =
+      http_response(200, "{\"access_token\": \"my-exchanged-gdch-token\"}");
   ExecCtx::Run(DEBUG_LOCATION, on_done, absl::OkStatus());
   return 1;
 }
 
-TEST_F(ExternalAccountCredentialsTest,
-       GDCHServiceAccountCredsSuccess) {
+TEST_F(ExternalAccountCredentialsTest, GDCHServiceAccountCredsSuccess) {
   ExecCtx exec_ctx;
   Json::Object obj = Json::Object{
       {"type", Json::FromString("gdch_service_account")},
@@ -4805,7 +4805,8 @@ TEST_F(ExternalAccountCredentialsTest,
 
 class GDCHServiceAccountCredentialsTest : public ::testing::Test {
  public:
-  static OrphanablePtr<GDCHServiceAccountCredentials::FetchBody> CallRetrieveSubjectToken(
+  static OrphanablePtr<GDCHServiceAccountCredentials::FetchBody>
+  CallRetrieveSubjectToken(
       GDCHServiceAccountCredentials* creds, Timestamp deadline,
       absl::AnyInvocable<void(absl::StatusOr<std::string>)> on_done) {
     return creds->RetrieveSubjectToken(deadline, std::move(on_done));
@@ -4841,7 +4842,8 @@ TEST_F(GDCHServiceAccountCredentialsTest, BasicRetrieveSubjectToken) {
       {"token_uri", Json::FromString("https://test-token-uri.com/token")},
   };
 
-  auto creds = GDCHServiceAccountCredentials::Create(Json::FromObject(obj), "https://my-audience.com", event_engine_);
+  auto creds = GDCHServiceAccountCredentials::Create(
+      Json::FromObject(obj), "https://my-audience.com", event_engine_);
   ASSERT_TRUE(creds.ok()) << creds.status().ToString();
   ASSERT_NE(*creds, nullptr);
   EXPECT_EQ((*creds)->min_security_level(), GRPC_PRIVACY_AND_INTEGRITY);
@@ -4850,7 +4852,7 @@ TEST_F(GDCHServiceAccountCredentialsTest, BasicRetrieveSubjectToken) {
   HttpRequest::SetOverride(httpcli_get_should_not_be_called,
                            gdch_service_account_creds_httpcli_post_success,
                            httpcli_put_should_not_be_called);
-  
+
   absl::StatusOr<std::string> fetched_token;
   bool done = false;
   auto fetch_body = GDCHServiceAccountCredentialsTest::CallRetrieveSubjectToken(
@@ -4859,10 +4861,10 @@ TEST_F(GDCHServiceAccountCredentialsTest, BasicRetrieveSubjectToken) {
         fetched_token = std::move(result);
         done = true;
       });
-  
+
   event_engine_->TickUntilIdle();
   ExecCtx::Get()->Flush();
-  
+
   EXPECT_TRUE(done);
   ASSERT_TRUE(fetched_token.ok()) << fetched_token.status().ToString();
   EXPECT_EQ(*fetched_token, "my-exchanged-gdch-token");
@@ -4875,7 +4877,8 @@ MATCHER(AccessTokenIsSTSBearer, "access token is STS Bearer") {
 }
 
 // This test is designed to be run inside an GDCH Adhoc environment.
-TEST_F(GDCHServiceAccountCredentialsTest, RetrievesBearerTokenInAdhocEnvironemnt) {
+TEST_F(GDCHServiceAccountCredentialsTest,
+       RetrievesBearerTokenInAdhocEnvironemnt) {
   auto key_file_env = GetEnv("GRPC_TEST_GDCH_KEY_FILE");
   auto audience_env = GetEnv("GRPC_TEST_GDCH_AUDIENCE");
   if (!key_file_env.has_value() || !audience_env.has_value()) GTEST_SKIP();
@@ -4884,7 +4887,8 @@ TEST_F(GDCHServiceAccountCredentialsTest, RetrievesBearerTokenInAdhocEnvironemnt
   auto contents = std::string{std::istreambuf_iterator<char>{is}, {}};
   ASSERT_THAT(contents, Not(::testing::IsEmpty()));
 
-  auto creds = grpc_gdch_service_account_credentials_create(contents.c_str(), audience_env->c_str());
+  auto creds = grpc_gdch_service_account_credentials_create(
+      contents.c_str(), audience_env->c_str());
   ASSERT_THAT(creds, ::testing::NotNull());
 
   std::string token = grpc_test_fetch_oauth2_token_with_credentials(creds);
